44fa0e981baffcd17b2e20b7bb2ace01c7d9820b
Sudhanshu Rai <sudhanshu.rai@wisdmlabs.com>
2023-03-21 16:42:01 +0530

    added the plugin details in the main file

---
+++ post-details-email.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:       Post Details Email
+Plugin URI:        https://sudhanshu.wisdmlabs.net/
+Description:       Sends mail to admin of all the posts published in a day
+Version:           1.0.0
+Author:            Sudhanshu Rai
+Author URI:        https://sudhanshu.wisdmlabs.net/
+License:           GPL-2.0+
+License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+Text Domain:       pde
+Domain Path:       /languages
+*/
